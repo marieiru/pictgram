e29de8a80b388bdfe4acef7404a8b9d204061f3d
imamura <marieiru@outlook.jp>
2022-07-23 18:11:39 +0900

    use2

--- src/main/java/com/example/pictgram/controller/UsersController.java
+++ src/main/java/com/example/pictgram/controller/UsersController.java
@@ -17,50 +17,53 @@ import com.example.pictgram.entity.User.Authority;
 import com.example.pictgram.form.UserForm;
 import com.example.pictgram.repository.UserRepository;
 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Controller
 public class UsersController {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private UserRepository repository;
+	@Autowired
+	private UserRepository repository;
 
-    @GetMapping(path = "/users/new")
-    public String newUser(Model model) {
-        model.addAttribute("form", new UserForm());
-        return "users/new";
-    }
+	@GetMapping(path = "/users/new")
+	public String newUser(Model model) {
+		model.addAttribute("form", new UserForm());
+		return "users/new";
+	}
 
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
-  
- public String create(@Validated @ModelAttribute("form") UserForm form, BindingResult result, Model model, RedirectAttributes redirAttrs) {
-   
-    	String name = form.getName();
-        String email = form.getEmail();
-        String password = form.getPassword();
-        String passwordConfirmation = form.getPasswordConfirmation();
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
 
-        if (repository.findByUsername(email) != null) {
-            FieldError fieldError = new FieldError(result.getObjectName(), "email", "その E メールはすでに使用されています。");
-            result.addError(fieldError);
-        }
-        if (result.hasErrors()) {
-          
-        	 model.addAttribute("hasMessage", true);
-        	            model.addAttribute("class", "alert-danger");
-        	          model.addAttribute("message", "ユーザー登録に失敗しました。");
-        	
-        	return "users/new";
-        }
+	public String create(@Validated @ModelAttribute("form") UserForm form, BindingResult result, Model model,
+			RedirectAttributes redirAttrs) {
 
-        User entity = new User(email, name, passwordEncoder.encode(password), Authority.ROLE_USER);
-        repository.saveAndFlush(entity);
+		String name = form.getName();
+		String email = form.getEmail();
+		String password = form.getPassword();
+		String passwordConfirmation = form.getPasswordConfirmation();
 
-        model.addAttribute("hasMessage", true);
-               model.addAttribute("class", "alert-info");
-               model.addAttribute("message", "ユーザー登録が完了しました。");
-        
-        return "layouts/complete";
-    }
+		if (repository.findByUsername(email) != null) {
+			FieldError fieldError = new FieldError(result.getObjectName(), "email", "その E メールはすでに使用されています。");
+			result.addError(fieldError);
+		}
+		if (result.hasErrors()) {
+
+			model.addAttribute("hasMessage", true);
+			model.addAttribute("class", "alert-danger");
+			model.addAttribute("message", "ユーザー登録に失敗しました。");
+
+			return "users/new";
+		}
+
+		User entity = new User(email, name, passwordEncoder.encode(password), Authority.ROLE_USER);
+		repository.saveAndFlush(entity);
+
+		model.addAttribute("hasMessage", true);
+		model.addAttribute("class", "alert-info");
+		model.addAttribute("message", "ユーザー登録が完了しました。");
+
+		return "layouts/complete";
+	}
 }
--- src/main/java/com/example/pictgram/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/pictgram/service/UserDetailsServiceImpl.java
@@ -9,9 +9,16 @@ import org.springframework.stereotype.Service;
 import com.example.pictgram.entity.User;
 import com.example.pictgram.repository.UserRepository;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	private UserRepository repository;
+	
     @Autowired
     protected static Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
